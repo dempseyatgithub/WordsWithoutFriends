2981972cb72c76146b854c83457592970f9af426
James Dempsey <dempsey@tapas-software.net>
2014-03-08 15:55:02 -0600

    Use Dynamic Type for table text

--- WordsWithoutFriends/TPSWordListViewController.m
+++ WordsWithoutFriends/TPSWordListViewController.m
@@ -15,6 +15,20 @@
 
 @implementation TPSWordListViewController
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(contentSizeChanged:) name:UIContentSizeCategoryDidChangeNotification object:[UIApplication sharedApplication]];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)contentSizeChanged:(NSNotification *)note {
+    [self.tableView reloadData];
+}
+
 
 #pragma mark - Table view data source
 
@@ -28,6 +42,9 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     NSDictionary *wordRecord = [self.words objectAtIndex:indexPath.row];
+    
+    cell.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    cell.detailTextLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
     
     cell.textLabel.text = wordRecord[@"word"];
     cell.detailTextLabel.text = wordRecord[@"partOfSpeech"];
